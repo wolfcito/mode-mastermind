3bc957b4f1aa3ac9e1a23a5cf5306f9a16dd4dea
Dany Lasso <33032918+dflasso@users.noreply.github.com>
2023-12-18 06:58:47 -0500

    feat: courses sstructure (#4)
    * chore: new home page (#1)
    
    * chore: new ui home resources added
    
    * feat: ui icons
    
    * chore: fantasy footer haaa
    
    * chore: tailwind, theme updated
    
    * chore: update icon
    
    * chore: improve wallet connect style
    
    * chore: theming updated
    
    * chore: improve title
    
    * chore(wip): home page style
    
    * build: dependency updated
    
    * add classes to home
    
    * define structure format slides
    
    * desig classes flow
    
    * fixed global styles
    
    ---------
    
    Co-authored-by: Luis Fernando Ushiña <guffenix@gmail.com>

--- packages/nextjs/contexts/ContentDidacticById/interfaces.ts
+++ packages/nextjs/contexts/ContentDidacticById/interfaces.ts
@@ -1,5 +1,5 @@
 import React from "react"
-import { DidacticContentClasses } from "~~/components/carousel-didactic-content/interfaces";
+import { DidacticContentClasses, Quizz } from "~~/components/carousel-didactic-content/interfaces";
 
 export interface ContentDidacticByIdState {
     progress: number,
@@ -25,4 +25,37 @@ export interface ContentDidactic {
 export interface ContentDidacticByIdProviderProps {
     children: React.ReactNode;
     idContent: string | string[] | undefined;
+}
+
+export enum ContentDidacticSlideTypes {
+    INFORMATIVE, 
+    TEST,
+    BAGDET
+}
+
+export interface ContentDidacticSwiperSlide {
+    id: string;
+    type: ContentDidacticSlideTypes,
+    title: string;
+    img: {
+        path: string;
+        width: number;
+        heigth: number;
+    }
+    desciption: string;
+    quizz: Quizz,
+    badget: {
+        img: {
+            path: string,
+            width: number,
+            heigth: number
+        },
+        title: string;
+        description: string;
+        tags: string[]
+    }
+}
+
+export interface UseRunwayContentDidactic {
+    swiperSlides: ContentDidacticSwiperSlide[]
 }
