8937dbbb6551fade1214d70f2e12a507bd7955d3
Luis Fernando <guffenix@gmail.com>
2023-12-17 22:10:29 -0500

    chore(wip): info home

---
+++ packages/nextjs/constants/home-roadmap.constants.ts
@@ -0,0 +1,24 @@
+import { HomeRoadmapProps } from '~~/types'
+
+export const HOME_ROADMAP: HomeRoadmapProps[] = [
+  {
+    description:
+      'Challenge yourself with mind-boggling trivia, earn badges as you conquer each level, and strive for epic achievements.',
+    image: '/assets/mode/separator.png',
+  },
+  {
+    description:
+      'Challenge yourself with mind-boggling trivia, earn badges as you conquer each level, and strive for epic achievements.',
+    image: '/assets/mode/separator.png',
+  },
+  {
+    description:
+      'Challenge yourself with mind-boggling trivia, earn badges as you conquer each level, and strive for epic achievements.',
+    image: '/assets/mode/separator.png',
+  },
+  {
+    description:
+      'Challenge yourself with mind-boggling trivia, earn badges as you conquer each level, and strive for epic achievements.',
+    image: '/assets/mode/separator.png',
+  },
+]
