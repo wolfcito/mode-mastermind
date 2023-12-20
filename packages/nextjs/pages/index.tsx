98410236e281f6ee1b04ceac2f2341ec0d1c8b6d
Luis Fernando <guffenix@gmail.com>
2023-12-20 18:35:55 -0500

    fix: remove useless imports

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,11 +1,10 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { nanoid } from 'nanoid'
+import { Slider } from '~~/components/animation'
 import { BadgeButton } from '~~/components/button'
 import { MetaHeader } from '~~/components/header'
 import { HOME_ROADMAP } from '~~/constants'
-import { BadgeButton } from '~~/components/button'
-import { Slider } from '~~/components/animation'
 import { ModeIcon } from '~~/icons'
 
 export default function Home() {
@@ -15,15 +14,21 @@ export default function Home() {
       <main className="flex flex-col items-center flex-grow pt-10 pb-60">
         <div className="px-5 w-[90%] md:w-[75%]">
           <h1 className="w-[750px] mt-24 mx-auto mb-6 text-center">
-            <span className="text-5xl font-press">LEARN <span className='block mt-1 mb-4'><ModeIcon /></span> AND UNLOCK RARE BADGES</span>
+            <span className="text-5xl font-press">
+              LEARN{' '}
+              <span className="block mt-1 mb-4">
+                <ModeIcon />
+              </span>{' '}
+              AND UNLOCK RARE BADGES
+            </span>
           </h1>
 
-          <div className='flex justify-center mt-3 mb-6'>
-            <BadgeButton label=" Play Now" onClick={function (): void {
-              console.log("Play Now")
-            } } />
+          <div className="flex justify-center mt-3 mb-6">
+            <Link href="/trivia" className="flex justify-center mb-6">
+              <BadgeButton label="Play now" />
+            </Link>
           </div>
-        
+
           {HOME_ROADMAP.map(item => (
             <div className="flex flex-col items-center justify-center" key={nanoid()}>
               <div className="flex flex-col items-center justify-center max-w-lg">
@@ -42,8 +47,8 @@ export default function Home() {
             </div>
           ))}
         </div>
-        <div className='pointer-events-none w-full h-2/3 bg-gradient-to-t from-black to-transparent fixed bottom-0 flex items-end'>
-          <Slider/>
+        <div className="fixed bottom-0 flex items-end w-full pointer-events-none h-2/3 bg-gradient-to-t from-black to-transparent">
+          <Slider />
         </div>
       </main>
     </>
