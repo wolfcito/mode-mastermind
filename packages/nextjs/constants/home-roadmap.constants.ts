47f55ce70885eb0cbc979a1f32ccc9e1c9a6b521
Joshua <66284362+kohasummons@users.noreply.github.com>
2023-12-21 02:18:54 +0000

    chore: update content

--- packages/nextjs/constants/home-roadmap.constants.ts
+++ packages/nextjs/constants/home-roadmap.constants.ts
@@ -1,24 +1,30 @@
+import { AchievementButton } from '~~/components/button'
 import { HomeRoadmapProps } from '~~/types'
 
 export const HOME_ROADMAP: HomeRoadmapProps[] = [
   {
     description:
-      "At Mode Mastermind, we're on a mission to build a brighter future. We believe education is key, both for our minds and our planet",
+      "At Mode Mastermind, we're on a mission to build a brighter future. We believe education is key, both for our minds and our planet.",
     image: '/assets/mode/separator-one.png',
   },
   {
     description:
-      'Challenge yourself with mind-boggling trivia, earn badges as you conquer each level, and strive for epic achievements.',
+      'The first step is to connect your wallet and challenge yourself with quick paced trivia, earn badges as you conquer each level, strive for epic achievements and ultimately become a Mode Master.',
     image: '/assets/mode/separator-two.png',
   },
   {
     description:
-      'Challenge yourself with mind-boggling trivia, earn badges as you conquer each level, and strive for epic achievements.',
+      'Each and every trivia presents a light reading section followed by a quiz. Conquer that quiz and take a step closer to the ultimate goal.',
     image: '/assets/mode/separator-three.png',
   },
   {
     description:
-      'Challenge yourself with mind-boggling trivia, earn badges as you conquer each level, and strive for epic achievements.',
+      "Once you've claimed three badges, you will be granted the power to unlock an epic Achievement. ",
+    image: '/assets/mode/separator-one.png',
+  },
+  {
+    description:
+      "Go and Become the Mode Master you were meant to be!",
     image: '/assets/mode/separator-one.png',
   },
 ]
