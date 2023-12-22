d907da18f948c0b528d3e5777a6cc1958b0d1733
Luis Fernando <guffenix@gmail.com>
2023-12-22 09:02:15 -0500

    chore(impr): performance optimization

--- packages/nextjs/pages/_document.tsx
+++ packages/nextjs/pages/_document.tsx
@@ -1,17 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
-    return (
-        <Html lang="en">
-            <Head>
-                <link rel="stylesheet" href="@sweetalert2/theme-dark/dark.css" />
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="stylesheet" href="@sweetalert2/theme-dark/dark.css" />
 
-                <script src="sweetalert2/dist/sweetalert2.min.js"></script>
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    )
+        <script src="sweetalert2/dist/sweetalert2.min.js" async></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
--- packages/nextjs/pages/achievement.tsx
+++ packages/nextjs/pages/achievement.tsx
@@ -1,21 +1,10 @@
-import { useAccount } from 'wagmi'
 import { MetaHeader } from '~~/components/header'
-import { RainbowKitCustomConnectButton } from '~~/components/scaffold-eth'
 import { MyHoldings } from '~~/components/simple-nft'
 
 export default function MyAchievements() {
-  const { isConnected, isConnecting } = useAccount()
-
   return (
     <>
       <MetaHeader />
-      {/* <div className="flex flex-col items-center pt-10">
-        <div className="px-5">
-          <h1 className="flex flex-col w-full mt-24 mb-6 text-center">
-            <span className="text-5xl font-press">Achievements</span>
-          </h1>
-        </div>
-      </div> */}
       <MyHoldings type={'achievement'} />
     </>
   )
